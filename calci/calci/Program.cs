990ff0b040c6db444d7322d96e4f508d421ce31d
A <saieshwar.a@siemens-healthineers.com>
2023-03-10 10:13:15 +0530

    console app added

---
+++ calci/calci/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Class1
+{
+    public float result;
+    public float calculate(string value, decimal valueFirst, decimal valueSecond)
+    {
+
+        switch (value)
+        {
+            case "+":
+                result = (float)(valueFirst + valueSecond);
+                break;
+            case "-":
+                result = (float)(valueFirst - valueSecond);
+                break;
+            case "*":
+
+                result = (float)(valueFirst * valueSecond);
+                break;
+            case "/":
+                result = (float)(valueFirst / valueSecond);
+                break;
+            //case "%":
+            //    result = (float)(valueFirst % valueSecond);
+            //    break;
+
+        }
+        return result;
+    }
+
+}
+namespace cal
+{
+   class Test
+    {
+        static void Main(string[] args)
+        {
+            Class1 x = new Class1();
+            float ans = x.calculate("+", 3, 4);
+            Console.WriteLine(ans);
+            Console.ReadLine();
+        }
+    }
+
+}
+
+
